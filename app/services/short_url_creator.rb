d26b6ee077ac2ef5975c680fbfee63875d17fd62
Shahid Nawaz <shahidjutt72@yahoo.com>
2021-11-30 18:25:07 +0500

    Rubocop run on ShortUrlCreator

--- app/services/short_url_creator.rb
+++ app/services/short_url_creator.rb
@@ -1,19 +1,17 @@
 class ShortUrlCreator
-  
   def initialize(long_url)
-  	@long_url = long_url
+    @long_url = long_url
   end
 
   def create!
-  	find_or_create
-  	"#{APP_URL}/short/#{@short_url.identifier}"
+    find_or_create
+    "#{APP_URL}/short/#{@short_url.identifier}"
   end
 
   protected
 
   def find_or_create
-  	@short_url = ShortUrl.where(long_url: @long_url).first_or_initialize(long_url: @long_url)  	
-  	@short_url.save
+    @short_url = ShortUrl.where(long_url: @long_url).first_or_initialize(long_url: @long_url)
+    @short_url.save
   end
-
 end
