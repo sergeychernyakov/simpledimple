1216e93695946937a71df0ba20b8a0d0d39189e9
Altruist2692 <altruist2692@gmail.com>
2021-12-05 10:46:31 +0530

    Make images as attachhment so that it can be accessible in mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: ENV['DEFAULT_FROM_EMAIL']
   layout 'mailer'
+  add_template_helper(EmailHelper)
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
-
 class UserMailer < ApplicationMailer
   def notification_email
+    attachments.inline["email-background.png"] = File.read("#{Rails.root}/app/assets/images/email-background.png")
     mail(to: ENV['ADMIN_EMAIL'], subject: 'Verification code sending failed')
   end
 end
